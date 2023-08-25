bca316129ac24c08d47e4ee140ee5a2030a00f64
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-25 12:52:47 +0530

    nested for nloop Task

---
+++ sarathi python task.py
@@ -0,0 +1,45 @@
+##Task 1
+##r=int(input("enter the rows"))
+##c=int(input("enter the columns"))
+##for i in range(1,r+1):
+##    for j in range(1,c+1):
+##        print(i,end=" ")
+##    print()
+##Task 2
+##r=int(input("enter the rows"))
+##c=int(input("enter the columns"))
+##for i in range(1,r+1):
+##    for j in range(1,c+1):
+##        print(j,end=" ")
+##    print()
+##Task 1
+##rows=int(input("enter the rows"))
+##for i in range(0,rows+1):
+##    for j in range(1,i):
+##        print(j,end=" ")
+##    print()
+##Task 2
+##r=int(input("enter the rows"))
+##for i in range(1,r+1):
+##    for j in range(0,i):
+##        print(i,end=" ")
+##    print()
+##Task 3
+r=int(input("enter the rows"))
+for i in range(r-1):
+    for j in range(r,i):
+        print(j,end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
